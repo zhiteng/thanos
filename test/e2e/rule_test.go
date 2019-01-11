8c4cc0c679256c7d1d1b74389b500f6973db92e7
Bartek Plotka <bwplotka@gmail.com>
2019-01-11 19:27:23 +0000

    Moved client prometheus code to separate promclient package.
    Nothing changed, except fixed IsWALDirAccessible (as WAL is actually a dir).
    Otherwise just moved code.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- test/e2e/rule_test.go
+++ test/e2e/rule_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/improbable-eng/thanos/pkg/promclient"
 	"github.com/improbable-eng/thanos/pkg/runutil"
 	"github.com/improbable-eng/thanos/pkg/testutil"
 	"github.com/pkg/errors"
@@ -127,7 +128,7 @@ func testRuleComponent(t *testing.T, conf testConfig) {
 		qtime := time.Now()
 
 		// The time series written for the firing alerting rule must be queryable.
-		res, err := queryPrometheus(ctx, "http://"+queryHTTP(1), time.Now(), "ALERTS", false)
+		res, err := promclient.QueryInstant(ctx, nil, urlParse(t, "http://"+queryHTTP(1)), "ALERTS", time.Now(), true)
 		if err != nil {
 			return err
 		}
